1b4e7ced020ad3967f3840a0c3b524ca06307649
NIKHILMOHAN063 <54111117+NIKHILMOHAN063@users.noreply.github.com>
2020-08-09 12:04:10 +0530

    Add files via upload

---
+++ Count Pairs-TCS.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+void test()
+{
+    ll n,k,l;
+    scanf("%lld",&n);
+
+    set<ll> s; // to remove duplicate elements from array
+    map<ll,int> m; // to keep track of frequency of each element in given array
+    
+    for(int i=0;i<n;i++) 
+    {scanf("%lld",&l);
+     s.insert(l);
+     m[l]++;
+    }
+
+   //copying the set to array a  
+   n=s.size();
+   ll a[n],i=0,count=0;
+
+   for(ll x : s) {a[i]=x;
+                  i++; }
+
+  
+  // loop to check  (adjacent diff <=k )
+
+  if(a[1]-a[0]<=k) count+=m[a[0]];
+  for(int i=1;i<n-1;i++)
+  {
+    if( a[i+1]-a[i]<=k || abs(a[i]-a[i-1])<=k) count+=m[a[i]]; // when this condition satisfies we increment the count by that element frequency.
+  }
+
+  if(a[n-1]-a[n-2]<=k) count+=m[a[n-1]];
+    
+
+   cout<<count; // printing output
+    
+}
+int main()
+{ test();  
+}
---
+++ Railway Stations-TCS.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve(ll a[],ll b[],int n) 
+{ 
+    sort(a,a+n); 
+    sort(b,b+n); 
+
+    int p=1,r=1; 
+    for(int i=1,j=0; i<n && j<n;) { 
+
+        if (a[i]<=b[j])
+        {p++; 
+         i++;} 
+ 
+        else if (a[i]>b[j])
+        { p--; 
+          j++;}  
+        r=max(r,p);
+    } 
+    return r; 
+} 
+
+void test()
+{
+    ll n;
+    scanf("%lld",&n);
+    
+    ll a[n],b[n],s,t;
+    
+    
+    for(int i=0;i<n;i++) 
+    {scanf("%lld%lld",&s,&t);
+     a[i]=s;
+     b[i]=s+t;
+    }
+    
+    cout<<solve(a,b,n);
+    
+}
+int main()
+{ test();  
+}
