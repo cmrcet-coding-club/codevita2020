6bf7b9b11b943298ad537bd918d599ee72945383
NIKHILMOHAN063 <54111117+NIKHILMOHAN063@users.noreply.github.com>
2020-08-09 21:43:05 +0530

    Update Count Pairs.cpp

--- Count Pairs.cpp
+++ Count Pairs.cpp
@@ -9,16 +9,17 @@ void test()
     ll n,k,l;
     scanf("%lld",&n);
 
-    set<ll> s; // to remove duplicate elements from array
+    set<ll> s; // to remove duplicate elements from array (we distinct elements only)
     map<ll,int> m; // to keep track of frequency of each element in given array
     
     for(int i=0;i<n;i++) 
     {scanf("%lld",&l);
-     s.insert(l);
-     m[l]++;
+     s.insert(l); //insertion in to set
+     m[l]++;      // counting frequency for element l
     }
 
-   //copying the set to array a  
+   //copying the set to array a(to acess previous, current and next element at a time)
+   // set by default sort the input , we dont need to sort the array
    n=s.size();
    ll a[n],i=0,count=0;
 
@@ -27,19 +28,22 @@ void test()
 
   
   // loop to check  (adjacent diff <=k )
-
-  if(a[1]-a[0]<=k) count+=m[a[0]];
-  for(int i=1;i<n-1;i++)
-  {
-    if( a[i+1]-a[i]<=k || abs(a[i]-a[i-1])<=k) count+=m[a[i]]; // when this condition satisfies we increment the count by that element frequency.
-  }
-
-  if(a[n-1]-a[n-2]<=k) count+=m[a[n-1]];
+     //since we dont have previous element for a[0] , we keep a separatle check for this (jst to avoid runtime error)
+     if(a[1]-a[0]<=k) count+=m[a[0]];
+     for(int i=1;i<n-1;i++)
+     {
+     // when this condition satisfies we increment the count by that element frequency.
+     if( a[i+1]-a[i]<=k || abs(a[i]-a[i-1])<=k) count+=m[a[i]]; 
+     }
+     //since we dont have next element for a[n-1] , we keep a separatle check for this (jst to avoid runtime error)
+     if(a[n-1]-a[n-2]<=k) count+=m[a[n-1]];
     
-
-   cout<<count; // printing output
+   // printing output
+   cout<<count; 
     
 }
+
+//driver code
 int main()
 { test();  
 }
